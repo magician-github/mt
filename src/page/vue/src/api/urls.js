6c8be249008a79dd64f79488867f206332049cfb
magician-gitgub <1589799055@qq.com>
2020-08-03 12:32:11 +0800

    修复文件

--- src/page/vue/src/api/urls.js
+++
@@ -1,9 +0,0 @@
-export default {
-    baseURL: 'https://open.duyiedu.com',
-    addStu: '/api/student/addStudent',
-    findByPage: '/api/student/findByPage',
-    updateStu: '/api/student/updateStudent',
-    delStu: '/api/student/delBySno',
-    searchStu: '/api/student/searchStudent'
-
-}
